07ce70d2100c01eadc81eb9718343f7a6bb1adc1
brulej <brule.jeremie@gmail.com>
2020-10-04 21:56:38 +0100

    initial import

---
+++ td00/src/main/java/fr/dauphine/ja/brulejeremie/td00/PrimeCollection.java
@@ -0,0 +1,56 @@
+package fr.dauphine.ja.brulejeremie.td00;
+
+import java.util.ArrayList;
+import java.util.Random;
+import java.util.Iterator;
+
+public class PrimeCollection 
+{ 
+	int toto;
+	String texte;
+	ArrayList<Integer> numbers;
+	
+	public PrimeCollection(ArrayList<Integer> numbers ) {
+		this.numbers = numbers;
+	}
+	public PrimeCollection() {
+		this.numbers = new ArrayList<Integer>() ;
+	}
+		
+	private void initRandom(int n,int m) {
+		Random random = new Random();
+		for (int i=0; i< n ; i++) {
+			this.numbers.add(random.nextInt(m));
+		}	
+	}
+	
+	
+	private boolean isPrime(int p) {
+		int sqrt = (int) Math.sqrt(p);
+		for (int i=2; i <= sqrt; i++) {
+			if (p%i == 0 ){
+				return false;
+			}
+		}
+		return true;
+	
+	}
+	
+	private void printPrimes() {
+		Iterator it = this.numbers .iterator();
+	    while(it.hasNext()) {
+	    	int elem = (int)it.next();
+	    	if ( isPrime(elem)== true ) {
+	    		System.out.println(elem);
+	    	}
+	      }	
+	}
+	
+    public static void main( String[] args )
+    {
+    	PrimeCollection collec = new PrimeCollection();
+    	collec.initRandom( 100, 1000);
+    	collec.printPrimes();
+        
+    }
+}
